c22b7b5bb5e5a712a47a1fd6cc683a8ab0005649
Mulamuleli Mammba <84395540+MulamuMammba@users.noreply.github.com>
2025-01-08 09:44:52 +0200

    initial commit:added project boilerplate

---
+++ src/main/java/com/mammba/APIManager/ApiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mammba.APIManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mammba/APIManager/Model/Users.java
@@ -0,0 +1,51 @@
+package com.mammba.APIManager.Model;
+
+
+public class Users {
+
+    private String email;
+    private String name;
+    private String surname;
+    private String profession;
+    private String phoneNum;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+}

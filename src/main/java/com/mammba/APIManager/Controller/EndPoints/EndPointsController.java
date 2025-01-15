084b07e40566f2c9fcec62ee0e441e62e2c32be1
Mulamuleli Mammba <84395540+MulamuMammba@users.noreply.github.com>
2025-01-15 13:40:07 +0200

    Updated: Endpoints can be tested as wel as standard CRUD functions

--- src/main/java/com/mammba/APIManager/Controller/EndPoints/EndPointsController.java
+++ src/main/java/com/mammba/APIManager/Controller/EndPoints/EndPointsController.java
@@ -1,60 +1,83 @@
 package com.mammba.APIManager.Controller.EndPoints;
 
 import com.mammba.APIManager.Controller.Auth.Authentication;
-import com.mammba.APIManager.Model.API;
-import com.mammba.APIManager.Services.EmailValidate;
+import com.mammba.APIManager.Model.Endpoints;
+import com.mammba.APIManager.Model.Response;
+import com.mammba.APIManager.Services.Security.EmailValidate;
 import com.mammba.APIManager.Services.Usecase.ApiUseCase;
 import com.mammba.APIManager.Services.Usecase.EndPointUseCase;
+import com.mammba.APIManager.Services.Usecase.TesterUseCase;
 import com.mammba.APIManager.Services.Usecase.UsersUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class EndPointsController {
 
     Authentication auth = new Authentication();
 
-    @GetMapping("/endpoint/list/{email}")
-    public ResponseEntity<Object> ApiList(@PathVariable String email) {
+    @PostMapping("/api/v1/endpoint/list")
+    public ResponseEntity<Object> ApiList(@RequestBody Map<String, String> requestBody) {
 
-        if (EmailValidate.isValid(email)) {
-            List<API> api = ApiUseCase.apiList(email);
-            return new ResponseEntity<>(api, HttpStatus.OK);
-        } else return new ResponseEntity<>("Invalid Password or Email", HttpStatus.OK);
-
-    }
-
-    @GetMapping("/endpoint/create/{email}/{name}")
-    public ResponseEntity<Object> CreateApi(@PathVariable String email, @PathVariable String name) {
+        String email = requestBody.get("userEmail");
+        String ApiId = requestBody.get("ApiId");
 
         if (!EmailValidate.isValid(email)) {
-            return new ResponseEntity<>("Invalid Password or Email", HttpStatus.OK);
+            return new ResponseEntity<>("Invalid Password or Email", HttpStatus.BAD_REQUEST);
 
+        } else if (!UsersUseCase.UserExists(email) || email == null || ApiId == null) {
+            return new ResponseEntity<>("User doesn't exist or doesn't have permission", HttpStatus.FORBIDDEN);
         } else {
-            ApiUseCase.CreateApi(email, name);
-            return new ResponseEntity<>("Successful", HttpStatus.OK);
+            List<Endpoints> endpoints = EndPointUseCase.EndPointList(ApiId);
+            return new ResponseEntity<>(endpoints, HttpStatus.OK);
         }
-
     }
 
-    @GetMapping("/endpoint/remove/{email}/{endPointId}")
-    public ResponseEntity<Object> RemoveApi(@PathVariable String email, @PathVariable String endPointId) {
+    @PostMapping("/api/v1/endpoint/test")
+    public ResponseEntity<Object> TestEndpoint(@RequestBody Map<String,String> requestBody){
 
+        Response data = TesterUseCase.TestEndpoint(requestBody.get("id"));
+        System.out.println(data.getBody());
+        return new ResponseEntity<>(data.getBody(), HttpStatus.OK);
+    }
+    @PostMapping("/api/v1/endpoint/create")
+    public ResponseEntity<Object> CreateEndpoint(@RequestBody Map<String, String> requestBody) {
+
+        System.out.println(requestBody);
+        String email = requestBody.get("userEmail");
+        Endpoints endpoint = new Endpoints("0", requestBody.get("apiId"), requestBody.get("name"), requestBody.get("url"), requestBody.get("method"));
+
+        System.out.println(endpoint);
         if (!EmailValidate.isValid(email)) {
-            return new ResponseEntity<>("Invalid Email", HttpStatus.OK);
+            return new ResponseEntity<>("Invalid Email", HttpStatus.BAD_REQUEST);
+
         } else if (!UsersUseCase.UserExists(email)) {
-            return new ResponseEntity<>("User does not exist", HttpStatus.OK);
-
-        } else if (!EndPointUseCase.EpiExists(endPointId)) {
-            return new ResponseEntity<>("Api Does not exist", HttpStatus.OK);
+            return new ResponseEntity<>("User doesn't exist or doesn't have permission", HttpStatus.FORBIDDEN);
         } else {
+            EndPointUseCase.CreateEndPoint(endpoint);
             return new ResponseEntity<>("Successful", HttpStatus.OK);
         }
 
     }
+
+    @GetMapping("/api/v1/endpoint/remove")
+    public ResponseEntity<Object> RemoveEndpoint(@RequestBody Endpoints endpoint) {
+        String email = ApiUseCase.FetchApi(endpoint.getApiId()).getUserEmail();
+        if (!EmailValidate.isValid(email)) {
+            return new ResponseEntity<>("Invalid Email", HttpStatus.BAD_REQUEST);
+        } else if (!UsersUseCase.UserExists(email)) {
+            return new ResponseEntity<>("User does not exist", HttpStatus.BAD_REQUEST);
+
+        }
+        else {
+            return new ResponseEntity<>("Successful", HttpStatus.OK);
+        }
+
+    }
+
 }
--- src/main/java/com/mammba/APIManager/Repository/EndpointsTable.java
+++ src/main/java/com/mammba/APIManager/Repository/EndpointsTable.java
@@ -3,6 +3,7 @@ package com.mammba.APIManager.Repository;
 import com.mammba.APIManager.Model.Endpoints;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EndpointsTable {
@@ -66,11 +67,59 @@ public class EndpointsTable {
     }
 
 
-    public static Endpoints getEndpointById(String id) {
-        return null;
+
+
+    public static List<Endpoints> getEndpointByApiId(String ApiId) {
+        String selectSQL = "SELECT * FROM Endpoints WHERE apiId = ?";
+        List<Endpoints> endpoint = new ArrayList<>(List.of());
+
+        try (Connection conn = DriverManager.getConnection(DATABASE_URL);
+             PreparedStatement pstmt = conn.prepareStatement(selectSQL)) {
+            pstmt.setString(1, ApiId);
+            ResultSet rs = pstmt.executeQuery();
+
+            endpoint.clear();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String url = rs.getString("url");
+                String method = rs.getString("method");
+
+                endpoint.add(new Endpoints(Integer.toString(id),ApiId,name,url,method));
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving api: " + e.getMessage());
+        }
+        return endpoint;
     }
 
-    public static List<Endpoints> getEndpointByUserEmail(String ApiId) {
-        return null;
+    public static Endpoints getEndpointById(String id) {
+        String selectSQL = "SELECT * FROM Endpoints WHERE id = ?";
+        Endpoints endpoint = new Endpoints(id," ","null",null,null);
+
+        try (Connection conn = DriverManager.getConnection(DATABASE_URL);
+             PreparedStatement pstmt = conn.prepareStatement(selectSQL)) {
+            pstmt.setInt(1, Integer.parseInt(id));
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                String apiId = rs.getString("apiId");
+                String name = rs.getString("name");
+                String url = rs.getString("url");
+                String method = rs.getString("method");
+
+                endpoint.setApiId(apiId);
+                endpoint.setName(name);
+                endpoint.setUrl(url);
+                endpoint.setMethod(method);
+                System.out.println(id);
+                System.out.println(endpoint);
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving api: " + e.getMessage());
+        }
+        return endpoint;
     }
 }
